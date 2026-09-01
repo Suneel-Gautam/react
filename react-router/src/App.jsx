ba834e2ac5d360b124ff9f7954cdcaad9d3202e9
Sunil Gautam <gautamsunil917@gmail.com>
2026-09-01 20:55:09 +0545

    updated code

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import Footer from "./components/footer/Footer";
+import { Router, Route } from "react-router-dom";
 function App() {
   return (
     <>
+  
       <Header />
       <Home />
+      <Footer />
     </>
   );
 }
